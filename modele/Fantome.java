6f204c1f578513be59ca96d1fd2f483afedae261
andgel-rib <andgelribeiro@gmail.com>
2020-04-08 15:44:00 +0200

    fantome ia tracker mode

--- modele/Fantome.java
+++ modele/Fantome.java
@@ -6,6 +6,7 @@
 package modele;
 
 import java.awt.Point;
+import java.util.HashSet;
 import java.util.Random;
 
 /**
@@ -23,15 +24,37 @@ public class Fantome extends Entite {
 
 	@Override
 	public void seDeplacer() {
-		switch (r.nextInt(2)) {
-			case 0:
-				this.direction = Direction.droite;
-				break;
-			case 1:
-				this.direction = Direction.bas;
-				break;
+		this.PacManHuntingMode();
+	}
+	
+	private void PacManHuntingMode() { // on va essayer de chasser pac man en choisissant une direction qui nous rapproche de lui
+		Point manPosition = this.jeu.getPacman().getPosition();
+		HashSet<Direction> availableDirections = this.getAvailableDirections();
+		int randomTired = this.r.nextInt(20);
+		Point pTest = new Point(this.position.x - manPosition.x,this.position.y - manPosition.y); // on soustrait la position du fantôme à celle de pacman pour décider de la direction
+		if(randomTired != 0 && pTest.x > 0 && availableDirections.contains(Direction.gauche) )
+			this.direction = Direction.gauche;
+		else if(randomTired != 0 && pTest.y < 0 && availableDirections.contains(Direction.bas))
+			this.direction = Direction.bas;
+		else if(randomTired != 0 && pTest.x < 0 && availableDirections.contains(Direction.droite))
+			this.direction = Direction.droite;
+		else if(randomTired != 0 && pTest.y > 0 && availableDirections.contains(Direction.haut))
+			this.direction = Direction.haut;
+		else {
+			int random = this.r.nextInt(availableDirections.size());
+			this.direction = (Direction)availableDirections.toArray()[random];
 		}
 		this.jeu.deplacerEntite(this, this.direction);
 	}
+	
+	private HashSet<Direction> getAvailableDirections(){
+		HashSet<Direction> availableDirections = new HashSet<Direction>();
+		for(Direction d : Direction.values()) {
+			if(this.jeu.checkDirectionWithPosition(this.position, d))
+			availableDirections.add(d);
+		}
+		return availableDirections;
+	}
+
 
 }

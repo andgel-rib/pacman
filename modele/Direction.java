4121ca1e5620ccb15e2e75289a99293028afe32c
Vinc Cholet <vincentl.cholet@gmail.com>
2020-04-15 18:09:23 +0200

    Update : trigger & murs

--- modele/Direction.java
+++ modele/Direction.java
@@ -7,7 +7,7 @@ package modele;
 
 /** Type énuméré des directions : les directions correspondent à un ensemble borné de valeurs, connu à l'avance
  *
- * @author freder
+ *
  */
 public enum Direction {
     haut, bas, gauche, droite
